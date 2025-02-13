c2984c32d6360061cd3349d150ce32bfe83b5f77
Samuele Converso <samu.converso@gmail.com>
2025-02-13 15:53:48 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio_U4_S1_L4/Models/Utente.cs
@@ -0,0 +1,38 @@
+﻿namespace Esercizio_U4_S1_L4.Models {
+    public static class Utente {
+
+        private static string username;
+        private static string password;
+        private static DateTime dateLogin;
+        private static List<DateTime> dates;
+        private static bool isLogged;
+
+        public static string Username {
+            get; set;
+        } = "";
+
+        public static string Password {
+            get; set;
+        } = "";
+
+        public static DateTime DateLogin {
+            get; set;
+        }
+
+        public static List<DateTime> Dates {
+            get; set;
+        } = new List<DateTime>();
+
+        public static bool IsLogged {
+            get; set;
+        } = false;
+
+        public static void Login(string username, string password, string confirmPassword) {
+            if (username != null && username != "" && username != " " && password != null && password != "" && password != " " && confirmPassword != null && confirmPassword != "" && confirmPassword != " " && password == confirmPassword) {
+                IsLogged = true;
+                DateLogin = DateTime.Now;
+                Dates.Add(DateLogin);
+            }
+        }
+    }
+}
